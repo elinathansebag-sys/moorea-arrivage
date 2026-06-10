e3d0740bbe1aae74fac14cb465291a99b1ada7f9
elinathansebag-sys <elinathan.sebag@moorea.fr>
2026-06-10 14:50:19 +0200

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, push, onValue, remove } from "firebase/database";
+import { getDatabase, ref, push, onValue, remove, update } from "firebase/database";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "firebase/auth";
 
 const firebaseConfig = {
@@ -16,4 +16,4 @@ const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
-export { ref, push, onValue, remove, signInWithPopup, signOut, onAuthStateChanged };
+export { ref, push, onValue, remove, update, signInWithPopup, signOut, onAuthStateChanged };
